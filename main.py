19bfc3a4a7ead2723a19afedd75c0a0e8d70e620
tejalsakore <sakoretejal1511@gmail.com>
2022-08-29 18:08:13 +0530

    gave final touch

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ input_frame = Frame(window,width = 312, height = 50,bd=0, highlightbackground="g
 input_frame.pack(side =TOP)
 
 # creating a input field inside the 'Frame'
-input_field = Entry(input_frame, font =('arial',18,'bold'), textvariable = input_text, width = 50, bg= "white",bd =0,justify=RIGHT)
+input_field = Entry(input_frame, font =('arial',18,'bold'), textvariable = input_text, width = 50, bg= "white",bd =0,highlightbackground="grey", highlightthickness=1,justify=RIGHT)
 input_field.grid(row = 0, column = 0)
 input_field.pack(ipady = 10) # 'ipady' is internal padding to increase the height of input field
 
@@ -50,21 +50,21 @@ clear = Button(btns_frame, text = "C", fg = "black", width = 32, height = 3, bd 
 divide = Button(btns_frame, text = "/", fg = "#fff", width = 10, height = 3, bd = 0 ,bg = "#393a3b", cursor = "hand2", command = lambda : btn_click("/")).grid(row=0,column=3,padx=1,pady=1)
 
 # second row
-seven = Button(btns_frame, text = "7", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(7)).grid(row=1,column=0,padx=1,pady=1)
-eight = Button(btns_frame, text = "8", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(8)).grid(row=1,column=1,padx=1,pady=1)
-nine = Button(btns_frame, text = "9", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(9)).grid(row=1,column=2,padx=1,pady=1)
+seven = Button(btns_frame, text = "7", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(7)).grid(row=1,column=0,padx=1,pady=1)
+eight = Button(btns_frame, text = "8", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(8)).grid(row=1,column=1,padx=1,pady=1)
+nine = Button(btns_frame, text = "9", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(9)).grid(row=1,column=2,padx=1,pady=1)
 multiply = Button(btns_frame, text = "*", fg = "#fff", width = 10, height = 3, bd = 0 ,bg = "#393a3b", cursor = "hand2", command = lambda : btn_click("*")).grid(row=1,column=3,padx=1,pady=1)
 
 # third row
-four = Button(btns_frame, text = "4", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(4)).grid(row=2,column=0,padx=1,pady=1)
-five = Button(btns_frame, text = "5", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(5)).grid(row=2,column=1,padx=1,pady=1)
-six = Button(btns_frame, text = "6", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(6)).grid(row=2,column=2,padx=1,pady=1)
+four = Button(btns_frame, text = "4", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(4)).grid(row=2,column=0,padx=1,pady=1)
+five = Button(btns_frame, text = "5", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(5)).grid(row=2,column=1,padx=1,pady=1)
+six = Button(btns_frame, text = "6", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(6)).grid(row=2,column=2,padx=1,pady=1)
 add = Button(btns_frame, text = "+", fg = "#fff", width = 10, height = 3, bd = 0 ,bg = "#393a3b", cursor = "hand2", command = lambda : btn_click("+")).grid(row=2,column=3,padx=1,pady=1)
 
 # fourth row
-one = Button(btns_frame, text = "1", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(1)).grid(row=3,column=0,padx=1,pady=1)
-two = Button(btns_frame, text = "2", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(2)).grid(row=3,column=1,padx=1,pady=1)
-three = Button(btns_frame, text = "3", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(3)).grid(row=3,column=2,padx=1,pady=1)
+one = Button(btns_frame, text = "1", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(1)).grid(row=3,column=0,padx=1,pady=1)
+two = Button(btns_frame, text = "2", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(2)).grid(row=3,column=1,padx=1,pady=1)
+three = Button(btns_frame, text = "3", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#bebfc2", cursor = "hand2", command = lambda : btn_click(3)).grid(row=3,column=2,padx=1,pady=1)
 sub = Button(btns_frame, text = "-", fg = "#fff", width = 10, height = 3, bd = 0 ,bg = "#393a3b", cursor = "hand2", command = lambda : btn_click("-")).grid(row=3,column=3,padx=1,pady=1)
 
 # last row
