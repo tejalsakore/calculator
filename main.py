b264ebafa2b5adbf73b6c982ebae8fa70e269fcc
tejalsakore <sakoretejal1511@gmail.com>
2022-08-29 17:29:01 +0530

    added second row

--- main.py
+++ main.py
@@ -48,4 +48,10 @@ btns_frame.pack()
 # first row
 clear = Button(btns_frame, text = "C", fg = "black", width = 32, height = 3, bd = 0 ,bg = "#eee", cursor = "hand2", command = lambda : btn_clear()).grid(row=0,column=0,columnspan =3,padx=1,pady=1)  
 divide = Button(btns_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#eee", cursor = "hand2", command = lambda : btn_click("/")).grid(row=0,column=3,padx=1,pady=1)
+
+# second row
+seven = Button(btns_frame, text = "7", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(7)).grid(row=1,column=0,padx=1,pady=1)
+eight = Button(btns_frame, text = "8", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(8)).grid(row=1,column=1,padx=1,pady=1)
+nine = Button(btns_frame, text = "9", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click(9)).grid(row=1,column=2,padx=1,pady=1)
+multiply = Button(btns_frame, text = "*", fg = "black", width = 10, height = 3, bd = 0 ,bg = "#fff", cursor = "hand2", command = lambda : btn_click("*")).grid(row=1,column=3,padx=1,pady=1)
 mainloop()
